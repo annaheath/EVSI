ffee189ccdf5ffeb4973496ac009089dc793cd7c
annaheath <31513217+annaheath@users.noreply.github.com>
2017-09-15 17:25:46 +0100

    Change legend
    Remove box from legend
    Further updates to come

--- R/enbs.plot.R
+++ R/enbs.plot.R
@@ -8,20 +8,20 @@ enbs.plot<-function(evsi,setup,pp,prob=NULL,
   ##'@param evsi An evsi object.
   ##'@param setup Gives the minimum and maximum possible setup costs of the trial.
   ##'@param pp Gives the minimum and maxiumum possible per person costs of the trial.
-  ##'@param prob The credible interval bounds that you would like plotted. 
+  ##'@param prob The credible interval bounds that you would like plotted.
   ##'    By default the CI intervals.
   ##'@param Pop The number of people benefitting from the treatment.
   ##'@param Time The minimum and maximum possible values for the time horizon of the treatment.
   ##'@param Dis The discount rate for future studies, based on NICE guidelines.
   ##'@param wtp The willingness to pay value that this analysis is being undetaken for.
   ##'   If NULL then the function will automatically select the central wtp to default in BCEA will be 25000
-  ##'@param N The sample sizes for which we want to consider the ENBS. If NULL taken as all the 
+  ##'@param N The sample sizes for which we want to consider the ENBS. If NULL taken as all the
   ##'   sample sizes that the EVSI has been calculated for.
   ##'@param pos The position where the legend will be printed in the resulting graph.
   ##'
   ##OUTPUTS:
   ##' @return A graphic that gives the ENBS for the range of N values given.
-  
+
   #Legend
   alt.legend <- pos
   if (is.numeric(alt.legend) & length(alt.legend) == 2) {
@@ -43,8 +43,8 @@ enbs.plot<-function(evsi,setup,pp,prob=NULL,
       alt.legend = "topright"
     else alt.legend = "topleft"
   }
-  
-  
+
+
   #Select WTP
   if(class(wtp)!="numeric"){
     wtp.select<-ceiling(length(evsi$attrib$wtp)/2)
@@ -53,35 +53,35 @@ enbs.plot<-function(evsi,setup,pp,prob=NULL,
   if(class(wtp)=="numeric"){
     wtp.select<-which.min((evsi$attrib$wtp-wtp)^2)
   }
-  
+
   #Select N
   if(class(N)!="numeric"){
     N<-evsi$attrib$N
   }
-  
+
   if(class(N)=="character"){
     stop("Please define the sample size of your experiment using N=")
   }
-  
+
   length.N<-length(N)
-  
+
   N.select<-array(NA,dim=length.N)
   for(i in 1:length.N){
     N.select[i]<-which.min((evsi$attrib$N-N[i])^2)
   }
-  
+
   #Select probabilities
   if(class(prob)!="numeric"){
     prob<-evsi$attrib$CI
   }
-  
+
   length.prob<-length(prob)
   #Select deteministic or probabilistic EVSI
   if(length(evsi$attrib$CI)==1){
     type.evsi<-"det"
     ##Select per person EVSI for plot
     evsi.params<-cbind(evsi$evsi[N.select,wtp.select,1],0)}
-  
+
   if(length(evsi$attrib$CI)>1){
     type.evsi<-"rand"
     q.1<-qnorm(evsi$attrib$CI[1])
@@ -92,24 +92,24 @@ enbs.plot<-function(evsi,setup,pp,prob=NULL,
       evsi.2<-evsi$evsi[N.select[i],wtp.select,length(evsi$attrib$CI)-1]
       evsi.params[i,]<-c((q.2*evsi.1-evsi.2*q.1)/(q.2-q.1),(evsi.2-evsi.1)/(q.2-q.1))}
   }
-  
-  
+
+
   ##Determine trial costs
   if(length(setup)!=2||length(pp)!=2){
     stop("Please give minimum and maximum values for the setup and per person trial costs.")}
-  
+
   ##Setting up the stochastic values for the trial costs
   setup.params<-c(mean(setup),(range(setup)[2]-range(setup)[1])/4)
   pp.params<-c(mean(pp),(range(pp)[2]-range(pp)[1])/4)
   trial.cost<-array(NA,dim=c(length.N, 2))
   for(i in 1:length.N){
     trial.cost[i,]<-c(setup.params[1]+pp.params[1]*N[i],sqrt(setup.params[2]^2+N[i]^2*pp.params[2]^2))}
-  
-  
-  
+
+
+
   #Colours for plotting
   colours<-colorRampPalette(c("black","navy","blue","skyblue","aliceblue","white"))(100)
-  
+
   ENBS<-function(evsi,Pop,Time,Dis,cost){
     #This calculation comes from a discounting of (exp(-Dis*Time)) but then you need to integrate over all
     #time points - hence divide by the discount rate and then multiply by some weird factor.
@@ -120,7 +120,7 @@ enbs.plot<-function(evsi,setup,pp,prob=NULL,
     var<- (Pop/Dis*(1-exp(-Dis*Time)))^2*evsi.sd^2+cost.sd^2
     return(sqrt(var))
   }
-  
+
   ENBS.mat<-array(NA,dim=c(length.N,5))
   for(j in 1:length.N){
     ENBS.mean<-ENBS(evsi.params[j,1],Pop,Time,Dis,trial.cost[j,1])
@@ -136,34 +136,35 @@ enbs.plot<-function(evsi,setup,pp,prob=NULL,
     lwd<-c(1:(length.prob/2),(length.prob/2):1,1)
     lty<-c((length.prob/2):1,2:(length.prob/2),1)
   }
-  
+
   plot(rep(N,length.prob),ENBS.mat,col="white",xlab="Sample Size",
        ylab="Expected Net Benefit of Sampling",oma=c(0,0,-1,0))
-  
+
   if(length.N<15){
     for(l in 1:length.prob){
       points(N,ENBS.mat[,l],pch=19,
              lwd=lwd[l],lty=lty[l])
     }
   }
-  
+
   if(length.N>=15){
     for(l in 1:length.prob){
       points(N,ENBS.mat[,l],type="l",
              lwd=lwd[l],lty=lty[l])
     }
   }
-  
+
   abline(h=0,col="springgreen",lwd=lwd[length.prob+1],lty=lty[length.prob+1])
-  
+
   legend(alt.legend,c(as.character(prob),"ENBS=0"),
-         col=c(rep("black",length.prob),"springgreen"),lwd=lwd,lty=lty)
-  
+         col=c(rep("black",length.prob),"springgreen"),lwd=lwd,lty=lty,
+         box.lwd = 0,box.col = "white",bg = "white")
+  box()
   optimal<-optim.ss(evsi,setup,pp,Pop,Time,wtp=wtp,Dis=Dis)
-  
+
   points(optimal$SS.max,optimal$ENBS,pch=4,lwd=2,cex=1.2,col="red2")
   points(optimal$SS.I,optimal$ENBS.I,pch="|",col="red2",cex=1.2,lty=3)
-  
+
 }
 
 
