eb6db7391065f64c9ea0c365f6dc3daffb750cd5
Anna Heath <31513217+annaheath@users.noreply.github.com>
2019-04-11 10:38:44 -0400

    Quadrature Updated for Correlated Data

--- R/data.quadrature.R
+++ R/data.quadrature.R
@@ -7,33 +7,45 @@ data.quadrature <- function(index.data, data, sample, Q, N){
   
   # Number of data sets
   dat.num <- sapply(index.data, length)
+  # Set up data objects
+  data.full <- list()
+  data.future <- vector("list",Q)
   
+  matrix.data.test <- (length(grep(",",colnames(sample[,unlist(index.data)]))) != 0)
+  cors <- sapply(1:min(dat.num),function(i){
+    cor(sample[,index.data[[1]][i]],sample[,index.data[[2]][i]])
+    })
+  correlation.test <- sign(quantile(cors, probs = c(0.025))) == sign(quantile(cors, probs = c(0.975)))
+  
+  if(matrix.data.test | correlation.test){
+    warning("Data generation within the EVSI package is not suitable for 
+            correlated data, please use the gen.quantiles function to
+            generate the data externally.")
+  }
   # Create a data matrix with only future data in it
-  data.mat <- sample[, unlist(index.data)]
-  # Determine a cluster allocation for the data points
-  cluster.allocation <- kmeans(data.mat, centers = Q, iter.max = 40)$cluster
-  # Extract the centre of each cluster
-  cluster.centre <- array(NA, dim = c(Q, sum(dat.num)))
+  for(l in 1:length(data)){
+  data.mat <- as.matrix(sample[, index.data[[l]]])
   categorical.check <- length(unique(as.numeric(as.matrix(data.mat))))
   
   if(categorical.check > 4){
-    for(l in 1:Q){
-      cluster.centre[l,] <- apply(data.mat[which(cluster.allocation == l), ], 2, mean)
+    data.quants <- apply(data.mat, 2, quantile,probs = 1:Q/(Q+1),type = 4)
+    cluster.centre <- array(NA, dim = c(Q, dat.num[l]))
+    for(i in 1:dim(data.mat)[2]){
+      cluster.centre[,i] <- sample(data.quants[,i],replace=FALSE)
     }
+
   }
   if(categorical.check <= 4){
-    for(l in 1:Q){
-      cluster.centre[l,] <- apply(data.mat[which(cluster.allocation == l), ], 2, sample, 1)
+    # Determine a cluster allocation for the data points
+    cluster.allocation <- kmeans(data.mat, centers = Q, iter.max = 40)$cluster
+    # Extract the centre of each cluster
+    cluster.centre <- array(NA, dim = c(Q, sum(dat.num)))
+
+    for(j in 1:Q){
+      cluster.centre[j,] <- apply(data.mat[which(cluster.allocation == j), ], 2, sample, 1)
     }
   }
-  
-  N.max <- max(N)
-  # Set up data objects
-  data.full <- list()
-  data.future<-vector("list",Q)
-  
-  for(l in 1:length(data)){
-    data.full[[l]] <- cluster.centre[, (((l - 1) * N.max) + 1):(l * N.max)] 
+    data.full[[l]] <- cluster.centre 
   }
   
   for(q in 1:Q){
@@ -42,6 +54,5 @@ data.quadrature <- function(index.data, data, sample, Q, N){
     }
     names(data.future[[q]])<-data
   }
-  
   return(data.future)
 }
